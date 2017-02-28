bbfde6799bd0d3821781d847ee37e3cbc2627341
SeanTobin94 <s.tobin94@gmail.com>
2017-02-28 15:33:36 +0000

    Hello again
    int y

--- EmailSite/EmailSite/RegisterPage.aspx.cs
+++ EmailSite/EmailSite/RegisterPage.aspx.cs
@@ -40,6 +40,7 @@ namespace EmailSite
 		{
 			String newName, newAdd, newMail, newAnswer, newPass;
             int i = 0;
+            int y = 0;
 			newName = TextBox1.Text;
 			newAdd = TextBox2.Text;
 			newMail = TextBox3.Text;
